a0ac9815eac718807b70ed70bf0c26284e952bf6
chipojaya1 <jaya@diveintocode.jp>
2020-09-30 03:08:03 +0000

    updating post function and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  delegate :email, to: :user, prefix: :author, allow_nil: false
+  validates :post, presence: true, length: {maximum: 500}
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :user_name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
 end
